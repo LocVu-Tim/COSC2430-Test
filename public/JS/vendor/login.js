5e5ec29cb72e8e21ec8cfd85473b64ef6b52e1af
Vu Loc <s3891483@rmit.edu.vn>
2023-05-10 20:12:30 +0700

    upload image tab update

--- public/JS/vendor/login.js
+++ public/JS/vendor/login.js
@@ -68,11 +68,22 @@ back.addEventListener("click", () => {
 //   });
 // });
 
-// Upload Profile Image
 
+// Upload and preview Profile Image
 let uploadFile = document.getElementById("upload-file");
 let profileImage = document.getElementById("profile-image");
 
 uploadFile.onchange = function () {
   profileImage.src = URL.createObjectURL(uploadFile.files[0]);
 };
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      document
+        .getElementById("profile-image")
+        .setAttribute("src", e.target.result);
+    };
+    reader.readAsDataURL(input.files[0]);
+  }
+}
