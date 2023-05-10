0ca37c9e94cae76f76c825f4b5ce3d6a5ee096d0
Vu Loc <s3891483@rmit.edu.vn>
2023-05-10 12:17:58 +0700

    login page update

--- public/JS/vendor/login.js
+++ public/JS/vendor/login.js
@@ -29,6 +29,23 @@ login.addEventListener("click", () => {
   container.classList.remove("active");
 });
 
+// sign up form
+const form = document.querySelector("form"),
+  nextBtn = form.querySelector(".nextBtn"),
+  backBtn = form.querySelector(".backBtn"),
+  allInput = form.querySelectorAll(".first input");
+nextBtn.addEventListener("click", () => {
+  allInput.forEach((input) => {
+    if (input.value != "") {
+      form.classList.add("secActive");
+    } else {
+      form.classList.remove("secActive");
+    }
+  });
+});
+backBtn.addEventListener("click", () => form.classList.remove("secActive"));
+
+// Validation
 const passwordInput = document.querySelector(".pass-field input");
 const eyeIcon = document.querySelector(".pass-field i");
 const requirementList = document.querySelectorAll(".requirement-list li");
