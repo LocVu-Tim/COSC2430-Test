d2440479ae692fa1a80628621e83f3ece8e187ba
Vu Loc <s3891483@rmit.edu.vn>
2023-05-10 17:30:12 +0700

    structure update

--- public/JS/vendor/login.js
+++ public/JS/vendor/login.js
@@ -31,9 +31,9 @@ login.addEventListener("click", () => {
 
 // sign up form
 const form = document.querySelector("form"),
-  nextBtn = form.querySelector(".nextBtn"),
-  backBtn = form.querySelector(".backBtn"),
-  allInput = form.querySelectorAll(".first input");
+  nextBtn = document.querySelector(".nextBtn"),
+  backBtn = document.querySelector(".backBtn"),
+  allInput = document.querySelectorAll(".first input");
 nextBtn.addEventListener("click", () => {
   allInput.forEach((input) => {
     if (input.value != "") {
@@ -46,33 +46,33 @@ nextBtn.addEventListener("click", () => {
 backBtn.addEventListener("click", () => form.classList.remove("secActive"));
 
 // Validation
-const passwordInput = document.querySelector(".pass-field input");
-const eyeIcon = document.querySelector(".pass-field i");
-const requirementList = document.querySelectorAll(".requirement-list li");
-// An array of password requirements with corresponding
-// regular expressions and index of the requirement list item
-const requirements = [
-  { regex: /.{8,}/, index: 0 }, // Minimum of 8 characters
-  { regex: /[0-9]/, index: 1 }, // At least one number
-  { regex: /[a-z]/, index: 2 }, // At least one lowercase letter
-  { regex: /[^A-Za-z0-9]/, index: 3 }, // At least one special character
-  { regex: /[A-Z]/, index: 4 }, // At least one uppercase letter
-];
-passwordInput.addEventListener("keyup", (e) => {
-  requirements.forEach((item) => {
-    // Check if the password matches the requirement regex
-    const isValid = item.regex.test(e.target.value);
-    const requirementItem = requirementList[item.index];
-    // Updating class and icon of requirement item if requirement matched or not
-    if (isValid) {
-      requirementItem.classList.add("valid");
-      requirementItem.firstElementChild.className = "fa-solid fa-check";
-    } else {
-      requirementItem.classList.remove("valid");
-      requirementItem.firstElementChild.className = "fa-solid fa-circle";
-    }
-  });
-});
+// const passwordInput = document.querySelector(".pass-field input");
+// const eyeIcon = document.querySelector(".pass-field i");
+// const requirementList = document.querySelectorAll(".requirement-list li");
+// // An array of password requirements with corresponding
+// // regular expressions and index of the requirement list item
+// const requirements = [
+//   { regex: /.{8,}/, index: 0 }, // Minimum of 8 characters
+//   { regex: /[0-9]/, index: 1 }, // At least one number
+//   { regex: /[a-z]/, index: 2 }, // At least one lowercase letter
+//   { regex: /[^A-Za-z0-9]/, index: 3 }, // At least one special character
+//   { regex: /[A-Z]/, index: 4 }, // At least one uppercase letter
+// ];
+// passwordInput.addEventListener("keyup", (e) => {
+//   requirements.forEach((item) => {
+//     // Check if the password matches the requirement regex
+//     const isValid = item.regex.test(e.target.value);
+//     const requirementItem = requirementList[item.index];
+//     // Updating class and icon of requirement item if requirement matched or not
+//     if (isValid) {
+//       requirementItem.classList.add("valid");
+//       requirementItem.firstElementChild.className = "fa-solid fa-check";
+//     } else {
+//       requirementItem.classList.remove("valid");
+//       requirementItem.firstElementChild.className = "fa-solid fa-circle";
+//     }
+//   });
+// });
 
 // Upload Profile Image
 
